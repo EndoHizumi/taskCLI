ded9476a07619e1575ceea53974b8370810a9e87
EndoHizumi <endohizumi0114@gmail.com>
2021-02-25 20:12:53 +0900

    wip

--- task/get_handler.py
+++ task/get_handler.py
@@ -4,4 +4,4 @@ import dataset
 def handle(args):
     db = dataset.connect("sqlite:///taskList.sqlite")
     task_list = db["taskList"]
-    return task_list.find()
+    return task_list.find(state=None)
