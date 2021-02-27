985e6131b4e7f9556ec946535ae7ccafb5961b98
Endo_Hizumi <endohizumi0114@gmail.com>
2021-02-27 21:43:06 +0900

    update: add to argument. startDay and endDay

--- task.py
+++ task.py
@@ -2,6 +2,15 @@ import argparse
 from task import add_handler
 from task import done_handler
 from task import show_handler
+from datetime import datetime
+
+
+def validate_date(arg_date):
+    try:
+        return datetime.now().strptime(arg_date, "%Y/%m/%d")
+    except ValueError:
+        msg = f"Not a valid date: '{arg_date}'."
+        raise argparse.ArgumentTypeError(msg)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -9,6 +18,8 @@ if __name__ == "__main__":
 
     add_parser = sub_parser.add_parser('add', help='Insert task to TaskList. If priority is omitted, priority medium is set.')
     add_parser.add_argument('-t', '--taskName', type=str, required=True)
+    add_parser.add_argument('-s', '--startDay', type=validate_date, help='date input format %%Y/%%m/%%d %%H:%%M:%%S')
+    add_parser.add_argument('-e', '--endDay', type=validate_date, help='date input format %%Y/%%m/%%d %%H:%%M:%%S')
     add_parser.set_defaults(func=add_handler.handle)
 
     done_parser = sub_parser.add_parser('done', help='Done to task')
--- task/add_handler.py
+++ task/add_handler.py
@@ -1,9 +1,17 @@
 import dataset
+from datetime import datetime
 
 
 def handle(args):
+    start_date = args.startDay if args.startDay else datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    end_date = args.endtDay if args.endDay else datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     db = dataset.connect("sqlite:///taskList.sqlite")
     task_list = db["taskList"]
     length = len(task_list)
-    task_list.insert({'id': length, 'taskName': args.taskName})
+    task_list.insert(
+        {
+            'startDt': start_date,
+            'endDt': end_date,
+        }
+    )
     return task_list.find_one(id=length)
