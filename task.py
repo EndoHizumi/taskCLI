56dff861f6782736662ae43ca3fd6e9d263a6adb
Endo_Hizumi <endohizumi0114@gmail.com>
2021-02-27 21:37:29 +0900

    modify: rename parser name

--- task.py
+++ task.py
@@ -7,16 +7,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     sub_parser = parser.add_subparsers()
 
-    find_parser = sub_parser.add_parser('add', help='Insert task to TaskList. If priority is omitted, priority medium is set.')
-    find_parser.add_argument('-t', '--taskName', required=True)
-    find_parser.set_defaults(func=add_handler.handle)
+    add_parser = sub_parser.add_parser('add', help='Insert task to TaskList. If priority is omitted, priority medium is set.')
+    add_parser.add_argument('-t', '--taskName', type=str, required=True)
+    add_parser.set_defaults(func=add_handler.handle)
 
-    emboss_parser = sub_parser.add_parser('done', help='Done to task')
-    emboss_parser.add_argument('-i', '--id', required=True)
-    emboss_parser.set_defaults(func=done_handler.handle)
+    done_parser = sub_parser.add_parser('done', help='Done to task')
+    done_parser.set_defaults(func=done_handler.handle)
 
-    status_parser = sub_parser.add_parser('show', help='show taskList')
-    status_parser.set_defaults(func=show_handler.handle)
+    show_parser = sub_parser.add_parser('show', help='show taskList')
+    show_parser.set_defaults(func=show_handler.handle)
 
     args = parser.parse_args()
     print(args.func(args))
