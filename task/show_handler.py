aebecf5b6f732b7dc21e035055ccd95050478d9c
Endo_Hizumi <endohizumi0114@gmail.com>
2021-03-04 01:43:02 +0900

    update: changed it to pretty table.

--- task/show_handler.py
+++ task/show_handler.py
@@ -1,4 +1,4 @@
-import texttable as ttb
+import prettytable
 import dataset
 
 
@@ -17,11 +17,9 @@ def handle(args):
 
 
 def draw_table(result_set):
-    table = ttb.Texttable(120)  # 表型作成(大きさは以下の情報から決める）
-    table.set_cols_align(["c", "c", "c", "c", "c", "c", "c", "c", "c", "c"])  # 文字の配置,たぶんl(左揃え）r（右揃え）c（中央）
-    table.set_cols_valign(["m", "m", "m", "m", "m", "m", "m", "m", "m", "m"])  # 表示属性
-    result_list = [list(result_set[0].keys())]
+    header = [list(result_set[0].keys())]
     contents = [list(item.values()) for item in result_set]
-    result_list.extend(contents)
-    table.add_rows(result_list)
-    return table.draw()
+    table = prettytable.PrettyTable(header[0])
+    for item in contents:
+        table.add_row(item)
+    return table
