a2b81c6d1a9cf2ae6d72b070cddfb76c538748ba
EndoHizumi <endohizumi0114@gmail.com>
2021-03-05 20:59:43 +0900

    update: add option -d

--- task/add_handler.py
+++ task/add_handler.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 def handle(args):
     start_date = args.startDay if args.startDay else datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    done = 1 if args.done else 0
+    end_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S") if args.done else None
     db = dataset.connect("sqlite:///taskList.sqlite")
     task_list = db["taskList"]
     length = len(task_list)
@@ -12,12 +14,12 @@ def handle(args):
             'id': length,
             'taskName': args.taskName,
             'startDt': start_date,
-            'endDt': None,
+            'endDt': end_date,
             'createAt': datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
             'updateAt': datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
             'project': args.project,
             'tag': args.context,
-            'state': 0,
+            'state': done,
             'priority': args.importance
         }
     )
