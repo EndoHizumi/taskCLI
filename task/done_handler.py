834b29e867ab25f6f636c0ce271b6a59b257f485
EndoHizumi <endohizumi0114@gmail.com>
2021-04-20 19:59:38 +0900

    update: done comand.

--- task/done_handler.py
+++ task/done_handler.py
@@ -5,5 +5,5 @@ def handle(args):
     db = dataset.connect("sqlite:///taskList.sqlite")
     end_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     task_list = db["taskList"]
-    task_list.update({'id': args.id, 'state': 1, 'endDt': end_date, 'updateAt': end_date}, ['id'])
+    task_list.update({'id': args.id, 'state': 1, 'endDt': end_date, 'updateAt': end_date, 'tag': 'done'}, ['id'])
     return task_list.find_one(id=args.id)
