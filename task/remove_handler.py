a04a0800b094a7cacba75a06bb67d197d94f5ac7
EndoHizumi <endohizumi0114@gmail.com>
2021-04-20 20:00:53 +0900

    wip

--- task/remove_handler.py
+++ task/remove_handler.py
@@ -1,9 +1,11 @@
 import dataset
-
+from pprint import pprint 
 
 def handle(args):
     db = dataset.connect("sqlite:///taskList.sqlite")
     task_list = db["taskList"]
-    target_row = task_list.find_one(id=args.id)
+
+    result_set = task_list.find_one(id=args.id)
+    target_row = [ item.get('id') for item in result_set]
     task_list.delete(id=args.id)
     return target_row
