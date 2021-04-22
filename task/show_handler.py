5cc488ae8918f5bfcb2cc1c2865f673d0251e210
Endo_Hizumi <endohizumi0114@gmail.com>
2021-04-23 01:07:35 +0900

    update: show command

--- task/show_handler.py
+++ task/show_handler.py
@@ -1,6 +1,7 @@
 import dataset
 from task import find_handler as find
 
+
 def handle(args):
     db = dataset.connect("sqlite:///taskList.sqlite")
     task_list = db["taskList"]
@@ -9,13 +10,14 @@ def handle(args):
     args_dict.pop('func')
     is_empty = not any(args_dict.values())
 
-    if args.all:
-        result_set = task_list.find()
-    elif args.doneOnly:
-        result_set = task_list.find(state=[1])
-    elif is_empty:
+    if is_empty:
         result_set = task_list.find(state=[None, 0])
     else:
-        result_set = find.handle(args)
+        if args.all:
+            result_set = task_list.find()
+        elif args.doneOnly:
+            result_set = [item for item in find.handle(args) if item['state'] == 1]
+        else:
+            result_set = [item for item in find.handle(args) if item['state'] == 0]
 
     return list(result_set)
