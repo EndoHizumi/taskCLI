6b28dd4e753f25b56a9e50c790ca00fa5ece6253
Endo_Hizumi <endohizumi0114@gmail.com>
2021-04-23 01:19:16 +0900

    update: exclude columns

--- task/show_handler.py
+++ task/show_handler.py
@@ -19,5 +19,9 @@ def handle(args):
             result_set = [item for item in find.handle(args) if item['state'] == 1]
         else:
             result_set = [item for item in find.handle(args) if item['state'] == 0]
+    result: list = []
+    for item in result_set:
+        del item['createAt'], item['updateAt']
+        result.append(item)
 
-    return list(result_set)
+    return list(result)
