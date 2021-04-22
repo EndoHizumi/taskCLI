9791a1b091011bd367cffaf04ae2c966f6874309
Endo_Hizumi <endohizumi0114@gmail.com>
2021-04-23 00:29:51 +0900

    update: add to reset command.

--- task/reset_handler.py
+++ task/reset_handler.py
@@ -3,8 +3,7 @@ from datetime import datetime
 
 def handle(args):
     db = dataset.connect("sqlite:///taskList.sqlite")
-    end_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    start_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     task_list = db["taskList"]
-    task_list.delete(id=args.id)
+    task_list.update({'id': args.id, 'state': 0, 'startDt': None, 'endDt': None, 'updateAt': start_date, 'tag': 'todo'}, ['id'])
     return task_list.find_one(id=args.id)
-
